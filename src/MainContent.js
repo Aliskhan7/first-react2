92ec1fb4719be751d148331dbf932b39a98654d2
juleybib <aliskhangazamatov@gmail.com>
2021-03-16 11:43:18 +0300

    react 4 dz

--- src/MainContent.js
+++ src/MainContent.js
@@ -1,11 +1,36 @@
 import Number from "./Number"
 import Buttons from "./Buttons"
+import {useState} from 'react'
 
 function MainContent(){
+
+    const [counter, setCounter] = useState(0)
+
+    function plus(){
+        setCounter(counter + 3)
+    }
+    function minus(){
+        if(counter < 2){
+            alert('Error');
+            setCounter(0)
+        }else{
+            setCounter(counter - 2)
+        }
+    }
+    function reset(){
+        setCounter(0)
+    }
+
     return(
         <div className='main-content'>
-       <Number/>
-       <Buttons/>
+      <div className='content-number'>
+           {counter}
+       </div>
+       <div className='buttons'>
+            <button onClick={plus}>Увеличить</button>
+            <button onClick={minus}>Уменьшить</button>
+            <button onClick={reset}>Сбросить</button>
+        </div>
        </div>
     )
 }
